529a10efdeafb84c2c82adeeed6c25484929adf5
Andres Saldarriaga Gaviria <jandreus87@hotmail.com>
2021-11-21 23:38:43 -0500

    fixing styles and functionality

--- src/index.js
+++ src/index.js
@@ -30,6 +30,11 @@ addEventListenerToElement(loanAmountBox, 'input', checkOnlyNumbers);
 addEventListenerToElement(annualTaxBox, 'input', checkOnlyNumbers);
 addEventListenerToElement(annualInsuranceBox, 'input', checkOnlyNumbers);
 
+/**
+ * Checks for empty fields and shows warning message
+ * @param {HTMLElement} element - the element to apply the error class.
+ * @param {HTMLElement} warningElement - Element to show below error.
+ */
 function checkEmptyBox(element, warningElement) {
     if (element.value === "$ " || element.value === "") {
         element.classList.add('main-container-inner__texts-group__loan-amount-box-error');
@@ -41,6 +46,11 @@ function checkEmptyBox(element, warningElement) {
         warningElement.classList.add('main-container-inner__texts-group__warning-hidden');
     }
 }
+
+/**
+ * Checks for the use of only numbers
+ * @param {event} e - js event to extract the key to get the ASCII code and only allow numbers.
+ */
 function checkOnlyNumbers(e) {
     const isNumber = e.data ? onlyNumberKey(e.data.charCodeAt(0)) : true;
     if (!isNumber) {
@@ -48,17 +58,23 @@ function checkOnlyNumbers(e) {
     }
 }
 
+/**
+ *  Sets the value of the slider to the value of the text box.
+ * @param {event} e - to use checkOnlyNumbers.
+ */
 function setYearSliderValue(e) {
-    const isNumber = e.data ? onlyNumberKey(e.data.charCodeAt(0)) : true;
-    if (!isNumber) this.value = "";
+    checkOnlyNumbers(e);
     if (this.value > 40) this.value = 40;
 
     yearsSlider.value = this.value
 }
 
+/**
+ *  Sets the value of the slider to the value of the text box.
+ * @param {event} e - to use checkOnlyNumbers..
+ */
 function setRateSliderValue(e) {
-    const isNumber = e.data ? onlyNumberKey(e.data.charCodeAt(0)) : true;
-    if (!isNumber) this.value = "";
+    checkOnlyNumbers(e);
     if (this.value > 10) {
         this.value = 10;
     }
@@ -66,47 +82,66 @@ function setRateSliderValue(e) {
     rateSlider.value = this.value;
 }
 
-calculateButton.addEventListener("click", function(e) {
+/**
+ *  Listener for the button to calculate all results.
+ */
+calculateButton.addEventListener("click", function() {
     checkEmptyBox(loanAmountBox, warningLoan);
     checkEmptyBox(annualTaxBox, warningTax);
     checkEmptyBox(annualInsuranceBox, warningInsurance);
+
     calculatePrincipalAndInterest();
     calculateMonthlyTax();
     calculateMonthlyInsurance();
     calculateMonthlyPayment();
+
     document.getElementById('results-container').classList.remove('main-container__results-hidden-mobile');
     document.getElementById('results-container').classList.add('main-container__results');
+    document.getElementById('principal-interest').classList.remove('main-container__results__cost-inactive');
+    document.getElementById('principal-interest').classList.add('main-container__results__cost');
+    document.getElementById('tax').classList.remove('main-container__results__cost-inactive');
+    document.getElementById('tax').classList.add('main-container__results__cost');
+    document.getElementById('monthly-insurance').classList.remove('main-container__results__cost-inactive');
+    document.getElementById('monthly-insurance').classList.add('main-container__results__cost');
+    document.getElementById('total-monthly').classList.remove('main-container__results__cost-inactive');
+    document.getElementById('total-monthly').classList.add('main-container__results__cost');
 }, false);
 
+/**
+ *  Helper function to add listeners to elements.
+ * @param {HTMLElement} element - the element to add the listener.
+ * @param {string} event - event to apply with the listener.
+ */
 function addEventListenerToElement(element, event, callback) {
     element.addEventListener(event, callback);
 }
 
+/**
+ * Transforms the ASCIIcode into a boolean indicating if it is a number.
+ * @param {number} ASCIICode - ASCIIcode from the key pressed.
+ */
 function onlyNumberKey(ASCIICode) {
     if (ASCIICode > 31  && (ASCIICode < 46 || ASCIICode > 57))
         return false;
     return true;
 }
 
+/**
+ * Calculates the principal and interest.
+ */
 function calculatePrincipalAndInterest() {
     const loanAmount = parseInt(loanAmountBox.value.replace('$', ''));
-    const annualTax = parseInt(annualTaxBox.value.replace('$', ''));
-    const annualInsurance = parseInt(annualInsuranceBox.value.replace('$', ''));
     const years = yearsSlider.value;
     const rate = rateSlider.value;
-
-    const monthlyTax = annualTax / 12;
-    const monthlyInsurance = annualInsurance / 12;
-
     const monthlyRate = rate / 100 / 12;
-
-    const monthlyPayment = (loanAmount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -years * 12));
-
-    const principalAndInterest = monthlyPayment - monthlyTax - monthlyInsurance;
+    const principalAndInterest = (loanAmount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -years * 12));
 
     document.getElementById('principal-interest').innerHTML = "$ " + principalAndInterest.toFixed(2);
 }
 
+/**
+ * Calculates the monthly tax.
+ */
 function calculateMonthlyTax() {
     const annualTax = parseInt(annualTaxBox.value.replace('$', ''));
     const monthlyTax = annualTax / 12;
@@ -114,6 +149,9 @@ function calculateMonthlyTax() {
     document.getElementById('tax').innerHTML = "$ " + monthlyTax.toFixed(2);
 }
 
+/**
+ * Calculates the monthly insurance.
+ */
 function calculateMonthlyInsurance() {
     const annualInsurance = parseInt(annualInsuranceBox.value.replace('$', ''));
     const monthlyInsurance = annualInsurance / 12;
@@ -121,6 +159,9 @@ function calculateMonthlyInsurance() {
     document.getElementById('monthly-insurance').innerHTML = "$ " + monthlyInsurance.toFixed(2);
 }
 
+/**
+ * Calculates the monthly payment.
+ */
 function calculateMonthlyPayment() {
     const principalAndInterest = parseInt(principalAndInterestBox.innerHTML.replace('$', ''));
     const monthlyTax = parseInt(monthlyTaxBox.innerHTML.replace('$', ''));
